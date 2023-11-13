b3a2fc9a5c580b6b352716ecad6e6e2a41ff6550
borocode <boroghor@icloud.com>
2023-11-12 20:31:15 -0500

    gave chatgpt the wheel

--- components/MintDetails.tsx
+++ components/MintDetails.tsx
@@ -1,10 +1,22 @@
 import { CollectionDetailsItem } from './CollectionDetails/CollectionDetailsItem';
 import * as Collapsible from '@radix-ui/react-collapsible';
-import { Box, Flex, Icon, Paragraph, Stack, Text } from '@zoralabs/zord';
+import { Box, Flex, Icon, Paragraph, Stack, StackProps, Text } from '@zoralabs/zord';
+import { dateOptions } from 'constants/dates';
+import { OPEN_EDITION_SIZE } from 'constants/numbers';
+import { useAllowlistEntry } from 'hooks/useAllowlistEntry';
+import { useSaleStatus } from 'hooks/useSaleStatus';
+import { ERC721DropProviderState } from 'providers/ERC721DropProvider';
+import React, { useMemo, useState } from 'react';
+import { collapsibleContent } from 'styles/styles.css';
 import { formatCryptoVal } from 'utils/numbers';
 import { useAccount } from 'wagmi';
 
-interface MintDetailsProps {
+// Placeholder for ERC721DropProviderState type
+interface ERC721DropProviderState {
+  // Define the structure of your type
+}
+
+interface MintDetailsProps extends StackProps {
   collection: ERC721DropProviderState;
   hideToggle?: boolean;
   initialOpen?: boolean;
@@ -14,21 +26,55 @@ export function MintDetails({
   collection,
   hideToggle,
   initialOpen = false,
+  ...props
 }: MintDetailsProps) {
   const { address } = useAccount();
+  const { allowlistEntry } = useAllowlistEntry({
+    merkleRoot: collection.salesConfig?.presaleMerkleRoot.toString(),
+    address,
+  });
 
+  const { totalMinted } = collection;
+  const { presaleExists, merkleRootExists, saleIsFinished, isSoldOut } = useSaleStatus({
+    collection,
+  });
+  const maxPerWallet = Number(collection.salesConfig.maxSalePurchasePerAddress);
+  const [opened, setOpened] = useState(initialOpen);
+
+  const startDate = useMemo(
+    () => new Date(Number(collection.salesConfig.publicSaleStart) * 1000),
+    [collection.salesConfig.publicSaleStart]
+  );
+  const endDate = useMemo(
+    () => new Date(Number(collection.salesConfig.publicSaleEnd) * 1000),
+    [collection.salesConfig.publicSaleEnd]
+  );
+
+  const presaleStartDate = useMemo(
+    () => new Date(Number(collection.salesConfig.presaleStart) * 1000),
+    [collection.salesConfig.presaleStart]
+  );
+  const presaleEndDate = useMemo(
+    () => new Date(Number(collection.salesConfig.presaleEnd) * 1000),
+    [collection.salesConfig.presaleEnd]
+  );
+
+  // TODO: handle integer overflows for when we do open mints
   const formattedMintedCount = Intl.NumberFormat('en', {
     notation: 'standard',
-  }).format(collection.totalMinted);
+  }).format(totalMinted || collection.totalMinted);
 
   const formattedTotalSupplyCount = Intl.NumberFormat('en', {
     notation: 'standard',
   }).format(collection.maxSupply);
 
   return (
-    <Stack gap="x4">
-      <Collapsible.Root defaultOpen={hideToggle} onOpenChange={(value) => setOpened(value)}>
-        <div className="zord-mint-details">
+    <Stack gap="x4" {...props}>
+      <Collapsible.Root
+        defaultOpen={hideToggle}
+        onOpenChange={(value) => setOpened(value)}
+      >
+        <Stack className="zord-mint-details" gap="x3">
           <CollectionDetailsItem className="zord-mint-details__item" name="Number minted">
             <Paragraph size="sm">
               {formattedMintedCount}
@@ -56,8 +102,7 @@ export function MintDetails({
           )}
 
           <Box>
-            <Collapsible.Content>
-              {/* Content of the collapsible section */}
+            <Collapsible.Content className={collapsibleContent}>
               <Stack gap="x3">
                 {!!(saleIsFinished || isSoldOut) && (
                   <CollectionDetailsItem
@@ -68,40 +113,6 @@ export function MintDetails({
                         ? 'Free'
                         : `${formatCryptoVal(collection.salesConfig.publicSalePrice)} ETH`
                     }
-                  />
-                )}
-
-                {presaleExists && merkleRootExists && (
-                  <>
-                    <CollectionDetailsItem
-                      className="zord-mint-details__item"
-                      name="Presale start"
-                      value={presaleStartDate.toLocaleString(...dateOptions)}
-                    />
-
-                    {!isNaN(presaleEndDate.getTime()) && (
-                      <CollectionDetailsItem
-                        className="zord-mint-details__item"
-                        name="Presale end"
-                        value={presaleEndDate.toLocaleString(...dateOptions)}
-                      />
-                    )}
-                  </>
-                )}
-
-                <CollectionDetailsItem
-                  className="zord-mint-details__item"
-                  name="Public sale start"
-                  value={startDate.toLocaleString(
-                    ...(dateOptions as [string, Intl.DateTimeFormatOptions])
-                  )}
-                />
-
-                {!isNaN(endDate.getTime()) && (
-                  <CollectionDetailsItem
-                    className="zord-mint-details__item"
-                    name="Public sale end"
-                    value={endDate.toLocaleString(...dateOptions)}
                   />
                 )}
               </Stack>
@@ -124,7 +135,7 @@ export function MintDetails({
               </Collapsible.Trigger>
             )}
           </Box>
-        </div>
+        </Stack>
       </Collapsible.Root>
     </Stack>
   );
