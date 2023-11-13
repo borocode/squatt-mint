334b0ecec4e647f8372cd18ce0217813747be151
borocode <boroghor@icloud.com>
2023-11-12 20:32:23 -0500

    back to normal

--- components/MintDetails.tsx
+++ components/MintDetails.tsx
@@ -1,25 +1,20 @@
-import { CollectionDetailsItem } from './CollectionDetails/CollectionDetailsItem';
-import * as Collapsible from '@radix-ui/react-collapsible';
-import { Box, Flex, Icon, Paragraph, Stack, StackProps, Text } from '@zoralabs/zord';
-import { dateOptions } from 'constants/dates';
-import { OPEN_EDITION_SIZE } from 'constants/numbers';
-import { useAllowlistEntry } from 'hooks/useAllowlistEntry';
-import { useSaleStatus } from 'hooks/useSaleStatus';
-import { ERC721DropProviderState } from 'providers/ERC721DropProvider';
-import React, { useMemo, useState } from 'react';
-import { collapsibleContent } from 'styles/styles.css';
-import { formatCryptoVal } from 'utils/numbers';
-import { useAccount } from 'wagmi';
-
-// Placeholder for ERC721DropProviderState type
-interface ERC721DropProviderState {
-  // Define the structure of your type
-}
+import { CollectionDetailsItem } from './CollectionDetails/CollectionDetailsItem'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Box, Flex, Icon, Paragraph, Stack, StackProps, Text } from '@zoralabs/zord'
+import { dateOptions } from 'constants/dates'
+import { OPEN_EDITION_SIZE } from 'constants/numbers'
+import { useAllowlistEntry } from 'hooks/useAllowlistEntry'
+import { useSaleStatus } from 'hooks/useSaleStatus'
+import { ERC721DropProviderState } from 'providers/ERC721DropProvider'
+import React, { useMemo, useState } from 'react'
+import { collapsibleContent } from 'styles/styles.css'
+import { formatCryptoVal } from 'utils/numbers'
+import { useAccount } from 'wagmi'
 
 interface MintDetailsProps extends StackProps {
-  collection: ERC721DropProviderState;
-  hideToggle?: boolean;
-  initialOpen?: boolean;
+  collection: ERC721DropProviderState
+  hideToggle?: boolean
+  initialOpen?: boolean
 }
 
 export function MintDetails({
@@ -28,45 +23,45 @@ export function MintDetails({
   initialOpen = false,
   ...props
 }: MintDetailsProps) {
-  const { address } = useAccount();
+  const { address } = useAccount()
   const { allowlistEntry } = useAllowlistEntry({
     merkleRoot: collection.salesConfig?.presaleMerkleRoot.toString(),
     address,
-  });
+  })
 
-  const { totalMinted } = collection;
+  const { totalMinted } = collection
   const { presaleExists, merkleRootExists, saleIsFinished, isSoldOut } = useSaleStatus({
     collection,
-  });
-  const maxPerWallet = Number(collection.salesConfig.maxSalePurchasePerAddress);
-  const [opened, setOpened] = useState(initialOpen);
+  })
+  const maxPerWallet = Number(collection.salesConfig.maxSalePurchasePerAddress)
+  const [opened, setOpened] = useState(initialOpen)
 
   const startDate = useMemo(
     () => new Date(Number(collection.salesConfig.publicSaleStart) * 1000),
     [collection.salesConfig.publicSaleStart]
-  );
+  )
   const endDate = useMemo(
     () => new Date(Number(collection.salesConfig.publicSaleEnd) * 1000),
     [collection.salesConfig.publicSaleEnd]
-  );
+  )
 
   const presaleStartDate = useMemo(
     () => new Date(Number(collection.salesConfig.presaleStart) * 1000),
     [collection.salesConfig.presaleStart]
-  );
+  )
   const presaleEndDate = useMemo(
     () => new Date(Number(collection.salesConfig.presaleEnd) * 1000),
     [collection.salesConfig.presaleEnd]
-  );
+  )
 
   // TODO: handle integer overflows for when we do open mints
   const formattedMintedCount = Intl.NumberFormat('en', {
     notation: 'standard',
-  }).format(totalMinted || collection.totalMinted);
+  }).format(totalMinted || collection.totalMinted)
 
   const formattedTotalSupplyCount = Intl.NumberFormat('en', {
     notation: 'standard',
-  }).format(collection.maxSupply);
+  }).format(collection.maxSupply)
 
   return (
     <Stack gap="x4" {...props}>
@@ -75,18 +70,28 @@ export function MintDetails({
         onOpenChange={(value) => setOpened(value)}
       >
         <Stack className="zord-mint-details" gap="x3">
-          <CollectionDetailsItem className="zord-mint-details__item" name="Number minted">
+          {/* <CollectionDetailsItem className="zord-mint-details__item" name="Number minted">
             <Paragraph size="sm">
               {formattedMintedCount}
               {collection.maxSupply > OPEN_EDITION_SIZE ? (
                 ' NFTs'
               ) : (
                 <Paragraph as="span" size="sm" color="tertiary">
-                  <span> / {formattedTotalSupplyCount}</span>
+      <span> / {formattedTotalSupplyCount}</span>
                 </Paragraph>
               )}
             </Paragraph>
-          </CollectionDetailsItem>
+          </CollectionDetailsItem> */}
+
+<CollectionDetailsItem style={{ margin: 0 }} className="zord-mint-details__item" name="Number minted">
+  <Paragraph size="sm" style={{ margin: 0 }}>
+    {formattedMintedCount}
+    {collection.maxSupply > OPEN_EDITION_SIZE && ' NFTs'}
+    {collection.maxSupply <= OPEN_EDITION_SIZE && (
+      <span style={{ margin: 0 }}>/ {formattedTotalSupplyCount}</span>
+    )}
+  </Paragraph>
+</CollectionDetailsItem>
 
           {maxPerWallet < OPEN_EDITION_SIZE && (
             <CollectionDetailsItem
@@ -94,6 +99,11 @@ export function MintDetails({
               name="Max per address"
             >
               <Text variant="paragraph-sm">
+                {/*userMintedCount && maxPerWallet < OPEN_EDITION_SIZE && (
+                <Box display="inline" color="tertiary">
+                  {userMintedCount}/
+                </Box>
+              )*/}
                 {allowlistEntry &&
                   `Presale: ${allowlistEntry.maxCanMint} / Public sale: `}
                 {maxPerWallet}
@@ -115,6 +125,40 @@ export function MintDetails({
                     }
                   />
                 )}
+
+                {/* {presaleExists && merkleRootExists && (
+                  <>
+                    <CollectionDetailsItem
+                      className="zord-mint-details__item"
+                      name="Presale start"
+                      value={presaleStartDate.toLocaleString(...dateOptions)}
+                    />
+
+                    {!isNaN(presaleEndDate.getTime()) && (
+                      <CollectionDetailsItem
+                        className="zord-mint-details__item"
+                        name="Presale end"
+                        value={presaleEndDate.toLocaleString(...dateOptions)}
+                      />
+                    )}
+                  </>
+                )}
+
+                <CollectionDetailsItem
+                  className="zord-mint-details__item"
+                  name="Public sale start"
+                  value={startDate.toLocaleString(
+                    ...(dateOptions as [string, Intl.DateTimeFormatOptions])
+                  )}
+                />
+
+                {!isNaN(endDate.getTime()) && (
+                  <CollectionDetailsItem
+                    className="zord-mint-details__item"
+                    name="Public sale end"
+                    value={endDate.toLocaleString(...dateOptions)}
+                  />
+                )} */}
               </Stack>
             </Collapsible.Content>
 
@@ -138,5 +182,5 @@ export function MintDetails({
         </Stack>
       </Collapsible.Root>
     </Stack>
-  );
+  )
 }
