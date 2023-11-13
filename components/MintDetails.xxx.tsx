2fea46afd219d6cd34169eb7d7de36cd432f6883
borocode <boroghor@icloud.com>
2023-11-12 20:26:59 -0500

    ;P

--- components/MintDetails.xxx.tsx
+++ components/MintDetails.xxx.tsx
@@ -1,46 +1,89 @@
-import { CollectionDetailsItem } from './CollectionDetails/CollectionDetailsItem';
-import * as Collapsible from '@radix-ui/react-collapsible';
-import { Box, Flex, Icon, Paragraph, Stack, Text } from '@zoralabs/zord';
-import { formatCryptoVal } from 'utils/numbers';
-import { useAccount } from 'wagmi';
+import { CollectionDetailsItem } from './CollectionDetails/CollectionDetailsItem'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Box, Flex, Icon, Paragraph, Stack, StackProps, Text } from '@zoralabs/zord'
+import { dateOptions } from 'constants/dates'
+import { OPEN_EDITION_SIZE } from 'constants/numbers'
+import { useAllowlistEntry } from 'hooks/useAllowlistEntry'
+import { useSaleStatus } from 'hooks/useSaleStatus'
+import { ERC721DropProviderState } from 'providers/ERC721DropProvider'
+import React, { useMemo, useState } from 'react'
+import { collapsibleContent } from 'styles/styles.css'
+import { formatCryptoVal } from 'utils/numbers'
+import { useAccount } from 'wagmi'
 
-interface MintDetailsProps {
-  collection: ERC721DropProviderState;
-  hideToggle?: boolean;
-  initialOpen?: boolean;
+interface MintDetailsProps extends StackProps {
+  collection: ERC721DropProviderState
+  hideToggle?: boolean
+  initialOpen?: boolean
 }
 
 export function MintDetails({
   collection,
   hideToggle,
   initialOpen = false,
+  ...props
 }: MintDetailsProps) {
-  const { address } = useAccount();
+  const { address } = useAccount()
+  const { allowlistEntry } = useAllowlistEntry({
+    merkleRoot: collection.salesConfig?.presaleMerkleRoot.toString(),
+    address,
+  })
 
+  const { totalMinted } = collection
+  const { presaleExists, merkleRootExists, saleIsFinished, isSoldOut } = useSaleStatus({
+    collection,
+  })
+  const maxPerWallet = Number(collection.salesConfig.maxSalePurchasePerAddress)
+  const [opened, setOpened] = useState(initialOpen)
+
+  const startDate = useMemo(
+    () => new Date(Number(collection.salesConfig.publicSaleStart) * 1000),
+    [collection.salesConfig.publicSaleStart]
+  )
+  const endDate = useMemo(
+    () => new Date(Number(collection.salesConfig.publicSaleEnd) * 1000),
+    [collection.salesConfig.publicSaleEnd]
+  )
+
+  const presaleStartDate = useMemo(
+    () => new Date(Number(collection.salesConfig.presaleStart) * 1000),
+    [collection.salesConfig.presaleStart]
+  )
+  const presaleEndDate = useMemo(
+    () => new Date(Number(collection.salesConfig.presaleEnd) * 1000),
+    [collection.salesConfig.presaleEnd]
+  )
+
+  // TODO: handle integer overflows for when we do open mints
   const formattedMintedCount = Intl.NumberFormat('en', {
     notation: 'standard',
-  }).format(collection.totalMinted);
+  }).format(totalMinted || collection.totalMinted)
 
   const formattedTotalSupplyCount = Intl.NumberFormat('en', {
     notation: 'standard',
-  }).format(collection.maxSupply);
+  }).format(collection.maxSupply)
 
   return (
-    <Stack gap="x4">
-      <Collapsible.Root defaultOpen={hideToggle} onOpenChange={(value) => setOpened(value)}>
-        <div className="zord-mint-details">
-          <CollectionDetailsItem className="zord-mint-details__item" name="Number minted">
+    <Stack gap="x4" {...props}>
+      <Collapsible.Root
+        defaultOpen={hideToggle}
+        onOpenChange={(value) => setOpened(value)}
+      >
+        <Stack className="zord-mint-details" gap="x3">
+           <CollectionDetailsItem className="zord-mint-details__item" name="Number minted">
             <Paragraph size="sm">
               {formattedMintedCount}
               {collection.maxSupply > OPEN_EDITION_SIZE ? (
                 ' NFTs'
               ) : (
                 <Paragraph as="span" size="sm" color="tertiary">
-                  <span> / {formattedTotalSupplyCount}</span>
+      <span> / {formattedTotalSupplyCount}</span>
                 </Paragraph>
               )}
             </Paragraph>
-          </CollectionDetailsItem>
+          </CollectionDetailsItem> 
+
+          
 
           {maxPerWallet < OPEN_EDITION_SIZE && (
             <CollectionDetailsItem
@@ -48,6 +91,11 @@ export function MintDetails({
               name="Max per address"
             >
               <Text variant="paragraph-sm">
+                {/*userMintedCount && maxPerWallet < OPEN_EDITION_SIZE && (
+                <Box display="inline" color="tertiary">
+                  {userMintedCount}/
+                </Box>
+              )*/}
                 {allowlistEntry &&
                   `Presale: ${allowlistEntry.maxCanMint} / Public sale: `}
                 {maxPerWallet}
@@ -56,8 +104,7 @@ export function MintDetails({
           )}
 
           <Box>
-            <Collapsible.Content>
-              {/* Content of the collapsible section */}
+            <Collapsible.Content className={collapsibleContent}>
               <Stack gap="x3">
                 {!!(saleIsFinished || isSoldOut) && (
                   <CollectionDetailsItem
@@ -71,7 +118,7 @@ export function MintDetails({
                   />
                 )}
 
-                {presaleExists && merkleRootExists && (
+                {/* {presaleExists && merkleRootExists && (
                   <>
                     <CollectionDetailsItem
                       className="zord-mint-details__item"
@@ -103,7 +150,7 @@ export function MintDetails({
                     name="Public sale end"
                     value={endDate.toLocaleString(...dateOptions)}
                   />
-                )}
+                )} */}
               </Stack>
             </Collapsible.Content>
 
@@ -124,8 +171,8 @@ export function MintDetails({
               </Collapsible.Trigger>
             )}
           </Box>
-        </div>
+        </Stack>
       </Collapsible.Root>
     </Stack>
-  );
+  )
 }
